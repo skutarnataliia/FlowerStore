542791a96b7ff1fda02590c769f4cd71f3cc8d9f
skutarnataliia <skutar.pn@ucu.edu.ua>
2023-10-12 18:16:58 +0300

    added classes Store, Bucket, Item

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -1,6 +1,22 @@
 package flower.store;
 
+import lombok.Getter;
+
+@Getter
 public class FlowerPack {
     private Flower flower;
-    private int quantity;
+    private double quantity;
+
+    public FlowerPack(Flower flower, double quantity) {
+        this.flower = flower;
+        this.quantity = quantity;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity = quantity > 0 ? quantity : 1;
+    }
+
+    public double getPrice() {
+        return flower.getPrice() * quantity;
+    }
 }
